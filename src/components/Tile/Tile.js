b32331c2a476a135dfbb3a270d0444c3e6aa01db
Roald Haverkamp <roald.haverkamp@amadeus.com>
2020-09-10 12:05:49 +0200

    Handling click events

--- src/components/Tile/Tile.js
+++ src/components/Tile/Tile.js
@@ -5,7 +5,7 @@ import './Tile.css'
 const Tile = (props) => {
   const dynamicColor = props.selected || props.matched ? { backgroundColor: props.color} : null
   return (
-    <div className='Tile' style={dynamicColor}>
+    <div className='Tile' style={dynamicColor} onClick={ () => { props.handleTileClicked(props.id, props.color); } }>
     { props.selected || props.matched ? <props.svg /> : null }
     </div>
   )
